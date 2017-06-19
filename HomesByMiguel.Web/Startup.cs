18ec49f8d59eafdf4091b83288e1a45d8e531e62
Miguel Gutierrez <miguel.g282@gmail.com>
2017-06-19 16:50:51 -0700

    initial commit

---
+++ HomesByMiguel.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomesByMiguel.Web.Startup))]
+namespace HomesByMiguel.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
